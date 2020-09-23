1e772b9ff333b14b6bc25da1f930d36ab4c5bab2
cameronb42 <70394521+cameronb42@users.noreply.github.com>
2020-09-23 13:00:10 -0500

    Update mainfile.py

--- mainfile.py
+++ mainfile.py
@@ -1,9 +1,31 @@
+# Grab file from the internet
+from urllib.request import urlretrieve
+
+URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+LOCAL_FILE = 'local_copy.log'
+
+# Use urlretrieve() to fetch a remote copy and save into the local file path
+local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
+
+# progress bar
+local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE, lambda x,y,z: print('.', end='', flush=True) if x % 100 == 0 else False)
+
+# open log
 FILE_NAME = '/home/cameron/local_copy.log'
 
-# Use open() to get a filehandle that can access the file
-fh = open(FILE_NAME)
+# Variables
+total_requests = 0
+totalyear_requests = 0
 
-# Loop through the file 
-for line in fh:
-  if re.match("1995
-  print(line)
+  
+# I chose to skip the filehandle like you mentioned in the  slack example
+for line in open(FILE_NAME):
+  total_requests += 1
+  if line.find("1994")!= -1:
+    totalyear_requests += 1
+
+# Print results
+print("Total Log Requests Made in the Last Year: ", totalyear_requests)
+
+print("Total Requests Made in Time Period of Log: ", total_requests)
+ 
